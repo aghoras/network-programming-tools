ea930314d6c14191588d5a7a56031b0e294d1ec7
Ali Ghorashi <ali.ghorashi@coloradoengineeringinc.com>
2014-03-14 11:34:29 -0600

    Added googletesting files for ease of use

---
+++ src/utils/testing/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ *
+ * @date   Mar 12, 2014
+ * @author ali
+ */
+
+
+#include "gtest.h"
+#include <cmath>
+#include <iostream>
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
